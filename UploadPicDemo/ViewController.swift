619e188bded052fb036c41ba4d2c3999ed33a227
huangwh <156952767@qq.com>
2019-04-03 09:15:39 +0800

    Initial Commit

---
+++ UploadPicDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UploadPicDemo
+//
+//  Created by soft on 2019/4/3.
+//  Copyright © 2019 hwh.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
