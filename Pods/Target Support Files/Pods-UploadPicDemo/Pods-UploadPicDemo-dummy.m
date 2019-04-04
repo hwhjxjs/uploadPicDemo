bca7b0d89d4d799a7c902b1a930b1dcf4090f75e
huangwh <156952767@qq.com>
2019-04-04 17:30:40 +0800

    first commit

---
+++ Pods/Target Support Files/Pods-UploadPicDemo/Pods-UploadPicDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UploadPicDemo : NSObject
+@end
+@implementation PodsDummy_Pods_UploadPicDemo
+@end
--- UploadPicDemo/ViewController.swift
+++ UploadPicDemo/ViewController.swift
@@ -1,20 +1,267 @@
+
 //
 //  ViewController.swift
-//  UploadPicDemo
+//  uploadDemo
 //
-//  Created by soft on 2019/4/3.
-//  Copyright © 2019 hwh.com. All rights reserved.
+//  Created by liwenban on 2017/8/21.
+//  Copyright © 2017年 hellomiao. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
+import MobileCoreServices
+import AssetsLibrary
+import AVKit
+import Alamofire
+import MediaPlayer
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    //设置标志，用于标识上传那种类型文件（图片／视频）
+    var flag = ""
+    //设置服务器地址
+    let uploadURL = "http://192.168.16.225/apitpl/api/web/index.php/tests/upload"
+//    let uploadURL = "http://192.168.16.225/upload.php"
 
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
+    
+    //按钮事件：上传图片
+    @IBAction func uploadImage(_ sender: Any) {
+        photoLib()
+//        load()
+    }
+    
+    //按钮事件：上传视频
+    @IBAction func uploadVideo(_ sender: Any) {
+        videoLib()
+    }
+    
+    //图库 - 照片
+    func photoLib(){
+        //
+        flag = "图片"
+        //判断设置是否支持图片库
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            //初始化图片控制器
+            let picker = UIImagePickerController()
+            //设置代理
+            picker.delegate = self
+            //指定图片控制器类型
+            picker.sourceType = UIImagePickerController.SourceType.photoLibrary
+            //弹出控制器，显示界面
+            self.present(picker, animated: true, completion: {
+                () -> Void in
+            })
+        }else{
+            print("读取相册错误")
+        }
+    }
+    
+    
+    //图库 - 视频
+    func videoLib(){
+        flag = "视频"
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            //初始化图片控制器
+            let imagePicker = UIImagePickerController()
+            //设置代理
+            imagePicker.delegate = self
+            //指定图片控制器类型
+            imagePicker.sourceType = .photoLibrary;
+            //只显示视频类型的文件
+            imagePicker.mediaTypes =  [kUTTypeMovie as String]
+            //不需要编辑
+            imagePicker.allowsEditing = false
+            //弹出控制器，显示界面
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        else {
+            print("读取相册错误")
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if flag == "视频" {
+            
+            //获取选取的视频路径
+            let videoURL = info[UIImagePickerController.InfoKey.mediaURL] as! URL
+            let pathString = videoURL.path
+            print("视频地址：\(pathString)")
+            //图片控制器退出
+            self.dismiss(animated: true, completion: nil)
+            let outpath = NSHomeDirectory() + "/Documents/\(Date().timeIntervalSince1970).mp4"
+            //视频转码
+            self.transformMoive(inputPath: pathString, outputPath: outpath)
+        }else{
+            //flag = "图片"
+            
+            //获取选取后的图片
+            let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+            //转成jpg格式图片
+            guard let jpegData = pickedImage.jpegData(compressionQuality:0.5) else {
+                return
+            }
+            //上传
+            self.uploadImage(imageData: jpegData)
+            //图片控制器退出
+            self.dismiss(animated: true, completion:nil)
+        }
+    }
+    
+    
+    
 
+    
+    //上传图片到服务器
+    func uploadImage(imageData : Data){
+        Alamofire.upload(
+            multipartFormData: { multipartFormData in
+                //采用post表单上传
+                // 参数解释：
+                //withName:和后台服务器的name要一致 ；fileName:可以充分利用写成用户的id，但是格式要写对； mimeType：规定的，要上传其他格式可以自行百度查一下
+                multipartFormData.append(imageData, withName: "imageFile", fileName: "123456.jpg", mimeType: "image/jpeg")
+                //如果需要上传多个文件,就多添加几个
+                //multipartFormData.append(imageData, withName: "file", fileName: "123456.jpg", mimeType: "image/jpeg")
+                //......
+                
+        },to: uploadURL,method: .post,encodingCompletion: { encodingResult in
+            switch encodingResult {
+            case .success(let upload, _, _):
+                //连接服务器成功后，对json的处理
+                upload.responseJSON { response in
+                    //解包
+                    guard let result = response.result.value else { return }
+                    print("json:\(result)")
+                }
+                //获取上传进度
+                upload.uploadProgress(queue: DispatchQueue.global(qos: .utility)) { progress in
+                    print("图片上传进度: \(progress.fractionCompleted)")
+                }
+            case .failure(let encodingError):
+                //打印连接失败原因
+                print(encodingError)
+            }
+        })
+    }
+    
+    //上传视频到服务器
+    func uploadVideo(mp4Path : URL){
+        Alamofire.upload(
+            //同样采用post表单上传
+            multipartFormData: { multipartFormData in
+                multipartFormData.append(mp4Path, withName: "imageFile", fileName: "1234560.mp4", mimeType: "video/mp4")
+                //服务器地址
+        },to: uploadURL,encodingCompletion: { encodingResult in
+            switch encodingResult {
+            case .success(let upload, _, _):
+                //json处理
+                upload.responseJSON { response in
+                    //解包
+                    guard let result = response.result.value else { return }
+                    print("json:\(result)")
+                }
+                //上传进度
+                upload.uploadProgress(queue: DispatchQueue.global(qos: .utility)) { progress in
+                    print("视频上传进度: \(progress.fractionCompleted)")
+                }
+            case .failure(let encodingError):
+                print(encodingError)
+            }
+        })
+    }
+    
+    /// 转换视频
+    ///
+    /// - Parameters:
+    ///   - inputPath: 输入url
+    ///   - outputPath:输出url
+    func transformMoive(inputPath:String,outputPath:String){
+        
+        
+        let avAsset:AVURLAsset = AVURLAsset(url: URL.init(fileURLWithPath: inputPath), options: nil)
+        let assetTime = avAsset.duration
+        
+        let duration = CMTimeGetSeconds(assetTime)
+        print("视频时长 \(duration)");
+        let compatiblePresets = AVAssetExportSession.exportPresets(compatibleWith: avAsset)
+        if compatiblePresets.contains(AVAssetExportPresetLowQuality) {
+            let exportSession:AVAssetExportSession = AVAssetExportSession.init(asset: avAsset, presetName: AVAssetExportPresetMediumQuality)!
+            let existBool = FileManager.default.fileExists(atPath: outputPath)
+            if existBool {
+            }
+            exportSession.outputURL = URL.init(fileURLWithPath: outputPath)
+            
+            
+            exportSession.outputFileType = AVFileType.mp4
+            exportSession.shouldOptimizeForNetworkUse = true;
+            exportSession.exportAsynchronously(completionHandler: {
+                
+                switch exportSession.status{
+                    
+                case .failed:
+                    
+                    print("失败...\(String(describing: exportSession.error?.localizedDescription))")
+                    break
+                case .cancelled:
+                    print("取消")
+                    break;
+                case .completed:
+                    print("转码成功")
+                    let mp4Path = URL.init(fileURLWithPath: outputPath)
+                    self.uploadVideo(mp4Path: mp4Path)
+                    break;
+                default:
+                    print("..")
+                    break;
+                }
+            })
+        }
+    }
+    
+    
+    
+    func load() {
+        print("开始上传")
+        let file = Bundle.main.path(forResource: "girl", ofType: "png")
+        let imageData = UIImage(contentsOfFile: file!)!.pngData()
+        
+        
+        Alamofire.upload(
+            multipartFormData: { multipartFormData in
+                //采用post表单上传
+                // 参数解释：
+                //withName:和后台服务器的name要一致 ；fileName:可以充分利用写成用户的id，但是格式要写对； mimeType：规定的，要上传其他格式可以自行百度查一下
+                multipartFormData.append(imageData!, withName: "imageFile", fileName: "girl.png", mimeType: "image/png")
+                //如果需要上传多个文件,就多添加几个
+                //multipartFormData.append(imageData, withName: "file", fileName: "123456.jpg", mimeType: "image/jpeg")
+                //......
+                
+        },to: uploadURL,method: .post,encodingCompletion: { encodingResult in
+            switch encodingResult {
+            case .success(let upload, _, _):
+                //连接服务器成功后，对json的处理
 
+                //获取上传进度
+                upload.uploadProgress(queue: DispatchQueue.global(qos: .utility)) { progress in
+                    print("图片上传进度: \(progress.fractionCompleted)")
+                }
+                upload.responseJSON { response in
+                    //解包
+                    guard let result = response.result.value else { return }
+                    print("json:\(result)")
+                }
+            case .failure(let encodingError):
+                //打印连接失败原因
+                print(encodingError)
+            }
+        })
+    }
+    
 }
-
